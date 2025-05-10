a502d7273c796b2c62e813ccb44226db232d5013
Lauiee <132264578+Lauiee@users.noreply.github.com>
2025-05-10 15:00:36 +0900

    fix: 출력 오타 수정

--- src/main/java/attendance/view/InputView.java
+++ src/main/java/attendance/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
         System.out.println("2. 출석 수정");
         System.out.println("3. 크루별 출석 기록 확인");
         System.out.println("4. 제적 위험자 확인");
-        System.out.println("0. 종료");
+        System.out.println("Q. 종료");
 
         return Console.readLine();
     }
